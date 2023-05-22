ce1ddca35adf44220c6be72f85a12904a4118b56
Chayenne <chayenne.leys@student.pxl.be>
2023-05-23 01:06:12 +0200

    winkelmand

--- winkelmand.js
+++ winkelmand.js
@@ -20,15 +20,40 @@ for (let i = 0; i < aantalInput.length; i++) {
 }
 
 //---------------------PRODUCT TOEVOEGEN -----------------------------
-let productButton= document.getElementsByClassName("product_item");
+let productButton = document.getElementsByClassName("product_item");
 for (let i = 0; i < productButton.length; i++) {
-    let button= productButton[i];
-    button.addEventListener("click", function (event){
-        let button= event.target
-        let productNaam= button.getElementsByClassName("product_benaming")[0].innerText;
-
+    let button = productButton[i];
+    button.addEventListener("click", function (event) {
+        let button = event.target
+        let productNaam = button.getElementsByClassName("product_benaming")[0].innerText;
+        let productPrijs = button.getElementsByClassName("product_prijs")[0].innerText;
+        let productFoto = button.getElementsByClassName("product_afbeelding")[0].src;
+        voegProductToe(productFoto, productNaam, productPrijs)
     })
     updateCartTotal()
+}
+
+function voegProductToe(productFoto, productNaam, productPrijs) {
+    let winkelmandRij = document.createElement("div");
+    winkelmandRij.classList.add("winkelmand_rij");
+    let winkelmandItem = document.getElementsByClassName("winkelmand_items")[0];
+    let inhoudRij = `<div class="col-2 winkelmand_foto align-middle">
+                        <img src="${productFoto}" alt="">
+                     </div>
+                     <div class="col-3 winkelmand_naam align-middle">
+                        <p>${productNaam}</p>
+                     </div>
+                     <div class="col-2 winkelmand_hoeveelheid align-middle">
+                        <input class="winkelmand_hoeveelheid_nummer" type="number" value="1" min="1" max="99">
+                     </div>
+                     <div class="col-2 winkelmand_prijs">
+                        <p>€<span class="winkelmand_prijs_nummer">${productPrijs}</span></p>
+                     </div>
+                     <div class="col-2 winkelmand_remove align-middle">
+                        <img src="assets/delete.png" alt="delete">
+                     </div>`
+    winkelmandRij.innerHTML = inhoudRij;
+    winkelmandItem.append(winkelmandRij);
 }
 
 
@@ -46,6 +71,6 @@ function updateCartTotal() {
         let hoeveelheid = hoeveelheidElement.value;
         totaal = totaal + (prijs * hoeveelheid);
     }
-    totaal= totaal.toFixed(2);
+    totaal = totaal.toFixed(2);
     document.getElementsByClassName('totale_prijs')[0].innerText = "€" + totaal;
 }
