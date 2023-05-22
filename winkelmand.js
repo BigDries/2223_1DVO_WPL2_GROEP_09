29ab0a53f761731c253d2ac1b41059db403233a7
Chayenne <chayenne.leys@student.pxl.be>
2023-05-22 20:11:52 +0200

    winkelmand

--- winkelmand.js
+++ winkelmand.js
@@ -1,24 +1,37 @@
-var removeItem = document.getElementsByClassName("winkelmand_remove");
-for (var i = 0; i < removeItem.length; i++) {
-    var button = removeItem[i]
+let removeItem = document.getElementsByClassName("winkelmand_remove");
+for (let i = 0; i < removeItem.length; i++) {
+    let button = removeItem[i];
     button.addEventListener("click", function (event) {
-        var buttonClicked = event.target
+        let buttonClicked = event.target
         buttonClicked.parentElement.parentElement.remove()
         updateCartTotal()
     })
 }
 
+let aantalInput = document.getElementsByClassName("winkelmand_hoeveelheid")[0];
+for (let i = 0; i < aantalInput.length; i++) {
+    let input = aantalInput[i];
+    input.addEventListener("change", function (event) {
+        let input = event.target;
+        if (isNaN(input.value) || input.value <= 0) {
+            input.value = 1;
+        }
+        updateCartTotal()
+    })
+}
+
 function updateCartTotal() {
-    var winkelmandContainer = document.getElementsByClassName("winkelmand_items")[0];
-    var winkelmandRij = winkelmandContainer.getElementsByClassName("winkelmand_rij");
-    var totaal = 0;
-    for (var i = 0; i < winkelmandRij.length; i++) {
-        var winkelmandItem = winkelmandRij[i];
-        var prijsElement = winkelmandItem.getElementsByClassName("winkelmand_prijs")[0];
-        var hoeveelheidElement = winkelmandItem.getElementsByClassName("winkelmand_hoeveelheid")[0];
-        var prijs = parseFloat(prijsElement.innerText.replace("€", ""));
-        var hoeveelheid = hoeveelheidElement.value;
+    let winkelmandContainer = document.getElementsByClassName("winkelmand_items")[0];
+    let winkelmandRij = winkelmandContainer.getElementsByClassName("winkelmand_rij")[0];
+    let totaal = 0;
+    for (let i = 0; i < winkelmandRij.length; i++) {
+        let winkelmandItem = winkelmandRij[i];
+        let prijsElement = winkelmandItem.getElementsByClassName("winkelmand_prijs")[0];
+        let hoeveelheidElement = winkelmandItem.getElementsByClassName("winkelmand_hoeveelheid")[0];
+        let prijs = parseInt(prijsElement.innerText.replace("€", ""));
+        let hoeveelheid = hoeveelheidElement.value;
         totaal = totaal + (prijs * hoeveelheid);
     }
-    document.getElementsByClassName('totale_prijs')[0].innerHTML = "<p>€</p>" + totaal;
+    totaal= Math.round(totaal * 100)/100;
+    document.getElementsByClassName('totale_prijs')[0].innerText = "€" + totaal;
 }
