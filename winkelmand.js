4644a6a03536c95815a593569529125df47708ed
Chayenne <chayenne.leys@student.pxl.be>
2023-05-22 20:45:45 +0200

    winkelmand

--- winkelmand.js
+++ winkelmand.js
@@ -8,16 +8,16 @@ for (let i = 0; i < removeItem.length; i++) {
     })
 }
 
-let aantalInput = document.getElementsByClassName("winkelmand_hoeveelheid")[0];
+let aantalInput = document.getElementsByClassName("winkelmand_hoeveelheid_number")[0];
 for (let i = 0; i < aantalInput.length; i++) {
     let input = aantalInput[i];
-    input.addEventListener("change", function (event) {
+    input.addEventListener = input.addEventListener("change", function (event) {
         let input = event.target;
         if (isNaN(input.value) || input.value <= 0) {
             input.value = 1;
         }
         updateCartTotal()
-    })
+    });
 }
 
 function updateCartTotal() {
@@ -26,12 +26,13 @@ function updateCartTotal() {
     let totaal = 0;
     for (let i = 0; i < winkelmandRij.length; i++) {
         let winkelmandItem = winkelmandRij[i];
-        let prijsElement = winkelmandItem.getElementsByClassName("winkelmand_prijs")[0];
-        let hoeveelheidElement = winkelmandItem.getElementsByClassName("winkelmand_hoeveelheid")[0];
+        let prijsElement = winkelmandItem.getElementsByClassName("winkelmand_prijs_nummer")[0];
+        let hoeveelheidElement = winkelmandItem.getElementsByClassName("winkelmand_hoeveelheid_number")[0];
         let prijs = parseInt(prijsElement.innerText.replace("€", ""));
         let hoeveelheid = hoeveelheidElement.value;
         totaal = totaal + (prijs * hoeveelheid);
     }
-    totaal= Math.round(totaal * 100)/100;
+    console.log(totaal);
+    //totaal= Math.round(totaal * 100)/100;
     document.getElementsByClassName('totale_prijs')[0].innerText = "€" + totaal;
 }
